c931b908f23e42d41ebe72a785dbe4c7a3fe9274
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-06-19 20:13:53 -0400

    Initial Commit

---
+++ src/main/java/supercoder79/chunksavingfix/ChunkSavingFix.java
@@ -0,0 +1,18 @@
+package supercoder79.chunksavingfix;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("chunksavingfix")
+public class ChunkSavingFix {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ChunkSavingFix() {
+
+    }
+}
---
+++ src/main/java/supercoder79/chunksavingfix/mixin/MixinChunkManager.java
@@ -0,0 +1,32 @@
+package supercoder79.chunksavingfix.mixin;
+
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimer;
+import net.minecraft.world.chunk.ChunkPrimerWrapper;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.server.ChunkHolder;
+import net.minecraft.world.server.ChunkManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Predicate;
+
+@Mixin(ChunkManager.class)
+public class MixinChunkManager {
+    // TODO: hits both at the moment- check and re-evaluate
+    @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 0), require = 0)
+    private Predicate<ChunkHolder> alwaysAccessibleFlush(Predicate<ChunkHolder> chunkHolder) {
+        return c -> true;
+    }
+
+    @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 1), require = 0)
+    private Predicate<IChunk> allowProtoChunkFlush(Predicate<IChunk> chunk) {
+        return c -> c instanceof ChunkPrimer || c instanceof ChunkPrimerWrapper || c instanceof Chunk;
+    }
+
+    @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 3), require = 0)
+    private Predicate<ChunkHolder> alwaysAccessible(Predicate<ChunkHolder> chunkHolder) {
+        return c -> true;
+    }
+}
